d29c7f7cc43394838cd7d1c567b0975d74e19647
Barsha <mebarsha1@gmail.com>
2019-11-13 07:30:07 +0545

    upper-lower

---
+++ day4.py
@@ -0,0 +1,11 @@
+
+user=str(input("enter the string"))
+string=input("enter string to be counted")
+upper=string.upper()
+lower=string.lower()
+big=(user.count(upper))
+small=(user.count(lower))
+Total=big+small
+print(Total)
+
+
